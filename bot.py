1a5899469e239b4e77d25b5e2de14ca48ed0b5e2
Marnik Merken <marnik.merken.mm@gmail.com>
2021-08-17 11:05:34 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,10 @@
+import os
 import discord
 from discord.ext import commands
+from dotenv import load_dotenv
 import aiohttp
+
+load_dotenv()
 
 bot = commands.Bot(command_prefix="$")
 
@@ -77,4 +81,4 @@ def get_rules():
             """
     return rules
 
-bot.run('ODY0NzYwODM0MzI0NjkzMDAy.YO6JSQ.1G_ZC6KUzwvRiPrO4RD8FglvOOQ')
+bot.run(os.getenv(('TOKEN')))
